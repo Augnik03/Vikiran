92a11c6aaa62a16f481414fc44877efc8873becd
Augnik <shresnik2004@gmail.com>
2024-02-26 11:10:38 +0530

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import { useState } from 'react'
+import Navbar from './components/Navbar'
+import Hero from './components/Hero'
+import Footer from './components/Footer'
+
+
+function App() {
+
+
+  return (
+    <>
+      <Hero></Hero>
+      <Navbar></Navbar>
+      <Footer></Footer>
+    </>
+  )
+}
+
+export default App
---
+++ src/components/Hero.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function Hero() {
+  return (
+    <div className='text-green-500'>Hero</div>
+  )
+}
