3ab145e8c56e9bf992193affb1b90caacb3075e1
Vedant Patil <vedant1346@gmail.com>
2024-03-01 01:44:35 +0530

    Home Page Dash

--- src/pages/DocHome.jsx
+++ src/pages/DocHome.jsx
@@ -1,8 +1,11 @@
-import React from 'react'
+import React from 'react';
+import Sidebar from '../components/Sidebar';
 
 export default function DocHome() {
   return (
-    <div>DocHome</div>
+    <div>DocHome
+      <Sidebar />
+    </div>
   )
 }
 
