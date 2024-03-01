dd944fff016242eba6dec46d3b79043b18ce0446
Vedant Patil <vedant1346@gmail.com>
2024-03-01 16:51:33 +0530

    Diagnose Page

--- src/pages/Diagnosis.jsx
+++ src/pages/Diagnosis.jsx
@@ -8,7 +8,9 @@ export default function Diagnosis() {
         <Sidebar />
       </div>
       <div className="p-4 sm:ml-64">
-        <div className="p-4 border-2 border-gray-200 border-dashed rounded-lg dark:border-gray-700 mt-14"></div>
+        <div className="p-4 border-2 border-gray-200 border-dashed rounded-lg dark:border-gray-700 mt-14">
+          <img src="./AI.jpeg" alt="" />
+        </div>
       </div>
     </>
   );
