b9524f8fc031c7b3ab97f1fb3d3572f17b5e15af
Augnik <shresnik2004@gmail.com>
2024-03-02 09:01:46 +0530

    Hugging face progress

--- src/utils/queryFunction.js
+++ src/utils/queryFunction.js
@@ -1,25 +1,19 @@
-import { HfInference } from "@huggingface/inference";
-
-
-
-const HF_ACCESS_TOKEN = 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;
-const inference = new HfInference(HF_ACCESS_TOKEN);
-
-// Define the model name
-const modelName = "nickmuchi/vit-finetuned-chest-xray-pneumonia";
-
-async function query(imageData) {
-  const response = await fetch(
-    `https://api-inference.huggingface.co/models/${modelName}`,
-    {
-      headers: { Authorization: `Bearer ${HF_ACCESS_TOKEN}` },
-      method: "POST",
-      body: imageData,
-    }
-  );
-
-  const result = await response.json();
-  return result;
+async function query(filename) {
+	const data = fs.readFileSync(filename);
+	const response = await fetch(
+		"https://api-inference.huggingface.co/models/nickmuchi/vit-finetuned-chest-xray-pneumonia",
+		{
+			headers: { Authorization: "Bearer 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" },
+			method: "POST",
+			body: data,
+		}
+	);
+	const result = await response.json();
+	return result;
 }
+
+query("cats.jpg").then((response) => {
+	console.log(JSON.stringify(response));
+});
 
 export default query;
