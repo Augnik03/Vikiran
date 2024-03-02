b53bb6ca4ee4fbb4ed65ae854b7d37760d09e0bb
piyushbaibhav <piyushbaibhav12@gmail.com>
2024-03-02 09:00:14 +0530

    landing page route change

--- src/pages/Diagnosis.jsx
+++ src/pages/Diagnosis.jsx
@@ -1,47 +1,47 @@
-import React, { useState } from 'react';
-import Sidebar from '../components/Sidebar';
-import queryFunction from '../utils/queryFunction';
+// import React, { useState } from 'react';
+// import Sidebar from '../components/Sidebar';
+// import queryFunction from '../utils/queryFunction';
 
-function Diagnosis() {
-  const [selectedFile, setSelectedFile] = useState(null);
+// function Diagnosis() {
+//   const [selectedFile, setSelectedFile] = useState(null);
 
-  const handleFileChange = (event) => {
-    setSelectedFile(event.target.files[0]);
-  };
+//   const handleFileChange = (event) => {
+//     setSelectedFile(event.target.files[0]);
+//   };
 
-  const handleQuery = async () => {
-    if (!selectedFile) {
-      alert("Please select a file.");
-      return;
-    }
+//   const handleQuery = async () => {
+//     if (!selectedFile) {
+//       alert("Please select a file.");
+//       return;
+//     }
 
-    const formData = new FormData();
-    formData.append("file", selectedFile);
+//     const formData = new FormData();
+//     formData.append("file", selectedFile);
 
-    try {
-      const response = await queryFunction(formData); 
-      console.log(response);
-    } catch (error) {
-      console.error("Error querying:", error);
-    }
-  };
+//     try {
+//       const response = await queryFunction(formData); 
+//       console.log(response);
+//     } catch (error) {
+//       console.error("Error querying:", error);
+//     }
+//   };
 
-  return (
-    <>
-      <div>
-        <Sidebar />
-      </div>
-      <div className="p-4 sm:ml-64">
-        <div className="p-4 border-2 border-gray-200 border-dashed rounded-lg dark:border-gray-700 mt-14">
-          <div>
-            <h1>My React Component</h1>
-            <input type="file" onChange={handleFileChange} />
-            <button onClick={handleQuery}>Query</button>
-          </div>
-        </div>
-      </div>
-    </>
-  );
-}
+//   return (
+//     <>
+//       <div>
+//         <Sidebar />
+//       </div>
+//       <div className="p-4 sm:ml-64">
+//         <div className="p-4 border-2 border-gray-200 border-dashed rounded-lg dark:border-gray-700 mt-14">
+//           <div>
+//             <h1>My React Component</h1>
+//             <input type="file" onChange={handleFileChange} />
+//             <button onClick={handleQuery}>Query</button>
+//           </div>
+//         </div>
+//       </div>
+//     </>
+//   );
+// }
 
-export default Diagnosis;
+// export default Diagnosis;
